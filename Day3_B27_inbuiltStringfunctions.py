63ee130cbeb0bfd2b1f4ba4ff10003448d521ae7
karishma1927 <karishmakash19@gmail.com>
2020-09-03 09:11:58 +0530

    Add files via upload

---
+++ Day3_B27_inbuiltStringfunctions.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+name='ravi samit'
+print(name.title())
+
+
+# In[5]:
+
+
+print(name.upper())
+
+
+# In[8]:
+
+
+print(name.lower())
+
+
+# In[11]:
+
+
+first_name='ravi'
+last_name='samit'
+full_name=f"{first_name} {last_name}"
+print(full_name)
+
+
+# In[12]:
+
+
+print(full_name.title())
+
+
+# In[15]:
+
+
+print(f'Hello, {full_name.title()}')
+
+
+# In[16]:
+
+
+print('fav languages:\njava\nc#\ncobol\npython')
+
